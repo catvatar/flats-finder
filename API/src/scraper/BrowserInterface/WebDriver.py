9ec3468454cc32050821264f03b3055a0c88386e
catvatar <olekgogol@protonmail.com>
2025-01-03 22:27:57 +0100

    refactored code to read like a newspaper

--- API/src/scraper/BrowserInterface/WebDriver.py
+++ API/src/scraper/BrowserInterface/WebDriver.py
@@ -8,7 +8,6 @@ class WebDriver:
     def __init__(self):
         self.options = Options()
 
-
     def start_browser_headless(self):
         self.__set_headless__()
         self.start_browser()
@@ -16,16 +15,6 @@ class WebDriver:
     def start_browser(self):
         self.__setOptions__()
         self.__create_Firefox_driver__()
-    
-    def __setOptions__(self):
-        self.options.set_preference('dom.webnotifications.enabled', False)
-        self.options.set_preference('media.webspeech.synth.enabled', False)
-
-    def __set_headless__(self):
-        self.options.add_argument('--headless')    
-
-    def __create_Firefox_driver__(self):
-        self.driver = webdriver.Firefox(options=self.options)
 
     def visit(self, url):
         self.driver.get(url)
@@ -40,7 +29,16 @@ class WebDriver:
     def get_cookie(self, name):
         return self.driver.get_cookie(name)
 
-
     def quit(self):
         self.driver.quit()
+    
+    def __setOptions__(self):
+        self.options.set_preference('dom.webnotifications.enabled', False)
+        self.options.set_preference('media.webspeech.synth.enabled', False)
+
+    def __set_headless__(self):
+        self.options.add_argument('--headless')    
+
+    def __create_Firefox_driver__(self):
+        self.driver = webdriver.Firefox(options=self.options)
 
