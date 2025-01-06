f0f48f5a7b5da1b566536e865cbb9bc72907472f
catvatar <olekgogol@protonmail.com>
2025-01-06 19:04:52 +0100

    WebDriver refactored to module singleton

--- API/src/scraper/BrowserInterface/WebDriver.py
+++ API/src/scraper/BrowserInterface/WebDriver.py
@@ -3,89 +3,86 @@ import selenium.webdriver.support as seleniumSupport
 import selenium.webdriver.support.wait as seleniumWait
 import selenium.webdriver.common.by as seleniumSelector
 
-class WebDriver:
-    options = None
-    driver = None
-    browserType = None
+options = None
+driver = None
+browserType = None
 
-    def __init__(self, browser='firefox'):
-        if browser == 'firefox':
-            self.browserType = 'firefox'
-            self.options = seleniumWebdriver.FirefoxOptions()
-        else:
-            pass
 
-    def start_browser_headless(self):
-        self.__set_headless__()
-        self.start_browser()
+def start_browser_headless():
+    __set_headless__()
+    start_browser()
 
-    def start_browser(self):
-        self.__setOptions__()
-        if self.browserType == 'firefox':
-            self.__create_Firefox_driver__()
+def start_browser():
+    __setOptions__()
+    if browserType == 'firefox':
+        __create_Firefox_driver__()
 
-    def visit(self, url):
-        self.driver.get(url)
+def visit(url):
+    driver.get(url)
 
-    def click_element_when_clickable(self,element):
-        seleniumWait.WebDriverWait(self.driver, 10).until(__element_is_clickable__(element)).click()
+def click_element_when_clickable(lement):
+    seleniumWait.WebDriverWait(driver, 10).until(__element_is_clickable__(element)).click()
+
+def type_to_element_by_id(id, keys):
+    find_element_by_id(id).send_keys(keys)
+
+def type_to_element_by_xpath(xpath, keys):
+    find_element_by_xpath(xpath).send_keys(keys)
+
+def click_element_by_id(id):
+    find_element_by_id(id).click()
+
+def click_element_by_xpath(xpath):
+    find_element_by_xpath(xpath).click()
     
-    def type_to_element_by_id(self, id, keys):
-        self.find_element_by_id(id).send_keys(keys)
+def click_element_by_css_selector(css_selector):
+    find_element_by_xpath(css_selector).click()        
 
-    def type_to_element_by_xpath(self, xpath, keys):
-        self.find_element_by_xpath(xpath).send_keys(keys)
+def find_element_by_id(id):
+    return __find_element__(seleniumSelector.By.ID, id)
 
-    def click_element_by_id(self, id):
-        self.find_element_by_id(id).click()
+def find_element_by_xpath(xpath):
+    return __find_element__(seleniumSelector.By.XPATH, xpath)
 
-    def click_element_by_xpath(self, xpath):
-        self.find_element_by_xpath(xpath).click()
-     
-    def click_element_by_css_selector(self, css_selector):
-        self.find_element_by_xpath(css_selector).click()        
+def find_element_by_css_selector(css_selector):
+    return __find_element__(seleniumSelector.By.CSS_SELECTOR, css_selector)        
 
-    def find_element_by_id(self, id):
-        return self.__find_element__(seleniumSelector.By.ID, id)
+def set_cookies(cookies):
+    for cookie in cookies:
+        set_cookie(cookie)
 
-    def find_element_by_xpath(self, xpath):
-        return self.__find_element__(seleniumSelector.By.XPATH, xpath)
-    
-    def find_element_by_css_selector(self, css_selector):
-        return self.__find_element__(seleniumSelector.By.CSS_SELECTOR, css_selector)        
+def set_cookie(cookie):
+    driver.add_cookie(cookie)
 
-    def set_cookies(self, cookies):
-        for cookie in cookies:
-            self.set_cookie(cookie)
+def get_cookie(name):
+    return driver.get_cookie(name)
 
-    def set_cookie(self, cookie):
-        self.driver.add_cookie(cookie)
+def quit():
+    driver.quit()
 
-    def get_cookie(self, name):
-        return self.driver.get_cookie(name)
+def __element_is_clickable__(element):
+    return seleniumSupport.expected_conditions.element_to_be_clickable(element)
 
-    def quit(self):
-        self.driver.quit()
+def __find_element__(By, param):
+    return driver.find_element(By, param)
 
-    def __element_is_clickable__(self, element):
-        return seleniumSupport.expected_conditions.element_to_be_clickable(element)
+def __setOptions__():
+    options.set_preference('dom.webnotifications.enabled', False)
+    options.set_preference('media.webspeech.synth.enabled', False)
 
-    def __find_element__(self, By, param):
-        return self.driver.find_element(By, param)
-    
-    def __setOptions__(self):
-        self.options.set_preference('dom.webnotifications.enabled', False)
-        self.options.set_preference('media.webspeech.synth.enabled', False)
+def __set_headless__():
+    options.add_argument('--headless')    
 
-    def __set_headless__(self):
-        self.options.add_argument('--headless')    
+def __create_Firefox_driver__():
+    driver = seleniumWebdriver.Firefox(options=options)
 
-    def __create_Firefox_driver__(self):
-        self.driver = seleniumWebdriver.Firefox(options=self.options)
+def performActionChain(actions):
+    action = seleniumWebdriver.ActionChains(driver)
+    for act in actions:
+        action.send_keys(act)
+    action.perform()
 
-    def performActionChain(self, actions):
-        action = seleniumWebdriver.ActionChains(self.driver)
-        for act in actions:
-            action.send_keys(act)
-        action.perform()
 
+if browser == 'firefox':
+    browserType = 'firefox'
+    options = seleniumWebdriver.FirefoxOptions()
