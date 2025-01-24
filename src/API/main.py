30edd058a3521f64d0786efaa0ab8419737c40e8
catvatar <olekgogol@protonmail.com>
2025-01-24 19:56:56 +0100

    new start

--- src/API/main.py
+++
@@ -1,7 +0,0 @@
-import Otodom
-
-def main():
-    Otodom.get_url_with_search_params()
-
-if __name__ == '__main__':
-    main()
--- src/WIP/Web_interface/__init__.py
+++
@@ -1,5 +0,0 @@
-from .browser_interface.Web_driver import Web_driver
-
-class HTML_downloader():
-    def __init__:
-        self.driver = webdriver
